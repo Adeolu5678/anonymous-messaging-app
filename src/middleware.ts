c23c4336669aa11833643413478d2d3f99661069
Emmanuel Adeoye <76078947+Adeolu5678@users.noreply.github.com>
2025-08-14 03:29:39 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,20 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: [
-    "/",
-    "/sign-in(.*)",
-    "/sign-up(.*)",
-  ],
+const isPublicRoute = createRouteMatcher([
+	"/",
+	"/sign-in(.*)",
+	"/sign-up(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+	if (isPublicRoute(req)) return;
+	auth().protect();
 });
 
 export const config = {
-  matcher: [
-    "/((?!_next|.*\\..*).*)", // skip static files and internals
-  ],
+	matcher: [
+		"/((?!.+\\.[\\w]+$|_next).*)",
+		"/",
+		"/(api|trpc)(.*)",
+	],
 };
